fbb18c5f7165adb88cc76d5a43e752f4c3ba7f3e
fontcala <adriafontc@gmail.com>
2019-05-08 13:20:41 +0200

    test new structures

--- test/firstTest.cpp
+++ test/firstTest.cpp
@@ -1,7 +1,20 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch2/catch.hpp"
 #include "libdl/hello.h"
+#include "libdl/mathutils.h"
 
-TEST_CASE( "first  tests", "[includes]" ) {
-    REQUIRE( hello("test") == 0 );
+TEST_CASE( "firsttests", "[includes]" ) {
+    REQUIRE( mathutils("test") == 0 );
 }
+
+TEST_CASE( "firsttests", "[includes]" ) {
+    REQUIRE( 0 == 0 );
+}
+
+// int main(){
+//     hello("tesst");
+// }
+
+// int main(){
+//      int a = 0;
+// }
