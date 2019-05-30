a7bc5247321df8afd0785c5dbd09fa533bbaa8a0
fontcala <adriafontc@gmail.com>
2019-05-30 10:00:19 +0200

    test how pybind works with classes

--- include/libdl/dlfunctions.h
+++ include/libdl/dlfunctions.h
@@ -8,10 +8,6 @@
 #include <Eigen/Core>
 //#include <spdlog/spdlog.h>
 
-// Question, is it a good idea to have for instance a header with activation functions and Loss functions and then eiter:
-// - pass these functions to the layer constructor.
-// - use these functions as template parameters of templated class layer.
-
 using Eigen::MatrixXd;
 
 namespace dlfunctions
--- python/main.cpp
+++ python/main.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
+#include <pybind11/eigen.h>
 #include <libdl/hello.h>
 
 
@@ -10,8 +11,21 @@ int hello_py(std::string const& name)
 {
   return hello(name);
 }
+using Eigen::MatrixXd;
+class MyClass {
+    MatrixXd someMat;
+public:
+    void setMatrix(const MatrixXd & aInput){someMat = aInput;}
+    const Eigen::MatrixXd &viewMatrix() { return someMat; }
+    
+};
 namespace py = pybind11;
 PYBIND11_MODULE(pybindings, m) {
     m.def("hello", &hello_py, "py");
     m.def("subtract", [](int i, int j) { return i - j; }, "substract");
+    py::class_<MyClass>(m, "MyClass")
+    .def(py::init<>())
+    .def("view_matrix", &MyClass::viewMatrix, py::return_value_policy::reference_internal)
+    .def("set_matrix", &MyClass::setMatrix,"set")
+    ;
 }
