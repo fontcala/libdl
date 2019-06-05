b42c9328f1c3717ce348173299d5722d03c9780c
fontcala <adriafontc@gmail.com>
2019-06-05 18:57:29 +0200

    add tests for the convolution and im2col (im2col image version)

---
+++ test/conv.cpp
@@ -0,0 +1,56 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
+#include "libdl/dlfunctions.h"
+
+TEST_CASE("conv", "convolution")
+{
+    // Input
+    MatrixXd InputVol1(16, 1);
+    MatrixXd InputVol2(16, 1);
+    MatrixXd InputVol3(16, 1);
+    InputVol3 << 301, 302, 303, 304, 305, 306, 307, 308, 309, 330, 311, 312, 313, 314, 315, 316;
+    InputVol1 << 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116;
+    InputVol2 << 201, 202, 203, 204, 205, 206, 207, 208, 209, 220, 211, 212, 213, 214, 215, 216;
+    MatrixXd InputVol(16, 3);
+    InputVol << InputVol1, InputVol2, InputVol3;
+
+    // Parameters
+    const size_t tFILTERSIZE = 3; // change also the template in the functions!
+    size_t vNumChannelsVol = 3;
+    size_t vImageHeightVol = 4;
+    size_t vImageWidthVol = 4;
+    size_t vStrideVol = 1;
+    size_t vPaddingVol = 1;
+    size_t vOutHeightVol = (vImageHeightVol - tFILTERSIZE + 2 * vPaddingVol) / vStrideVol + 1;
+    size_t vOutWidthVol = (vImageWidthVol - tFILTERSIZE + 2 * vPaddingVol) / vStrideVol + 1;
+    size_t vOutFieldsVol = tFILTERSIZE * tFILTERSIZE * vNumChannelsVol;
+    size_t vNumSamples = 1;
+
+    // Filters (Identity)
+    MatrixXd FilterVol1(vOutFieldsVol, 1);
+    MatrixXd FilterVol2(vOutFieldsVol, 1);
+    MatrixXd FilterVol3(vOutFieldsVol, 1);
+    FilterVol3 << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0;
+    FilterVol1 << 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+    FilterVol2 << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
+    MatrixXd FilterVol(vOutFieldsVol, 3);
+    FilterVol << FilterVol1, FilterVol2, FilterVol3;
+
+    SECTION("applying im2col manually")
+    {
+        MatrixXd OutputVol(vOutHeightVol * vOutWidthVol, vOutFieldsVol);
+        dlfunctions::im2col<3, 3>(InputVol.data(), OutputVol.data(), vOutHeightVol, vOutWidthVol, vOutFieldsVol, vImageWidthVol, vImageHeightVol, vNumChannelsVol, vPaddingVol, vPaddingVol, vStrideVol, vNumSamples);
+        MatrixXd OutputConv = OutputVol * FilterVol;
+
+        REQUIRE(OutputConv == InputVol);
+    }
+    SECTION("using the convolution function")
+    {
+        // Now using the conv function
+        size_t vFiltersNumber = 3;
+        MatrixXd OutputVolCnv(vOutHeightVol * vOutWidthVol, vFiltersNumber);
+        dlfunctions::convolution<3, 3>(OutputVolCnv, FilterVol, InputVol, vOutHeightVol, vOutWidthVol, vImageWidthVol, vImageHeightVol, vNumChannelsVol, vPaddingVol, vPaddingVol, vStrideVol, vNumSamples);
+
+        REQUIRE(OutputVolCnv == InputVol);
+    }
+}
---
+++ test/im2col.cpp
@@ -0,0 +1,59 @@
+#define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch2/catch.hpp"
+#include "libdl/dlfunctions.h"
+
+// TODO make scenarios for different stride, sizes etc...
+TEST_CASE("2D Data", "2D Data")
+{
+
+    // Template parameter vFilterSize = 2
+    MatrixXd Input(4, 4);
+    Input << 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16;
+    std::cout << "Input" << std::endl;
+    std::cout << Input << std::endl;
+
+    size_t vNumChannels = 1;
+    size_t vImageHeight = Input.rows();
+    size_t vImageWidth = Input.cols() / vNumChannels;
+    size_t vStride = 2;
+    size_t vOutHeight = (vImageHeight - 2) / vStride + 1;
+    size_t vOutWidth = (vImageWidth - 2) / vStride + 1;
+    MatrixXd Output(vOutHeight * vOutWidth, 2 * 2 * vNumChannels);
+    dlfunctions::im2col<2,2>(&Output, &Input, vStride, vNumChannels, vImageHeight, vImageWidth);
+
+    MatrixXd vExpectedOutput(vOutHeight * vOutWidth, 2 * 2 * vNumChannels);
+    vExpectedOutput << 1, 2, 5, 6, 3, 4, 7, 8, 9, 10, 13, 14, 11, 12, 15, 16;
+
+    REQUIRE(Output == vExpectedOutput);
+}
+
+TEST_CASE("3D Data", "3D Data")
+{
+
+    // Template parameter vFilterSize = 2
+    MatrixXd InputVol1(4, 4);
+    MatrixXd InputVol2(4, 4);
+    MatrixXd InputVol3(4, 4);
+    MatrixXd InputVol(4, 4 * 3);
+    InputVol3 << 301, 302, 303, 304, 305, 306, 307, 308, 309, 330, 311, 312, 313, 314, 315, 316;
+    InputVol1 << 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116;
+    InputVol2 << 201, 202, 203, 204, 205, 206, 207, 208, 209, 220, 211, 212, 213, 214, 215, 216;
+    InputVol << InputVol1, InputVol2, InputVol3;
+    std::cout << "InputVol" << std::endl;
+    std::cout << InputVol << std::endl;
+
+    size_t vNumChannelsVol = 3;
+    size_t vImageHeightVol = InputVol.rows();
+    size_t vImageWidthVol = InputVol.cols() / vNumChannelsVol;
+    size_t vStrideVol = 2;
+    size_t vOutHeightVol = (vImageHeightVol - 2) / vStrideVol + 1;
+    size_t vOutWidthVol = (vImageWidthVol - 2) / vStrideVol + 1;
+    MatrixXd OutputVol(vOutHeightVol * vOutWidthVol, 2 * 2 * vNumChannelsVol);
+    dlfunctions::im2col<2,2>(&OutputVol, &InputVol, vStrideVol, vNumChannelsVol,vImageHeightVol,vImageWidthVol);
+
+
+    MatrixXd vExpectedOutputVol(vOutHeightVol * vOutWidthVol, 2 * 2 * vNumChannelsVol);
+    vExpectedOutputVol << 101, 102, 105, 106, 201, 202, 205, 206, 301, 302, 305, 306, 103, 104, 107, 108, 203, 204, 207, 208, 303, 304, 307, 308, 109, 110, 113, 114, 209, 220, 213, 214, 309, 330, 313, 314, 111, 112, 115, 116, 211, 212, 215, 216, 311, 312, 315, 316;
+
+    REQUIRE(OutputVol == vExpectedOutputVol);
+}
