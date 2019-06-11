f0285d8f6056c8a3ad4afa572925b6b740c77026
fontcala <adriafontc@gmail.com>
2019-06-11 11:11:16 +0200

    Added softmax loss function

---
+++ examples/multipleoutputs.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include "libdl/FullyConnectedLayer.h"
+#include "libdl/SigmoidActivationLayer.h"
+#include "libdl/L2LossLayer.h"
+#include "libdl/SoftmaxLossLayer.h"
+
+int main()
+{
+    MatrixXd inputData(4, 2);
+    inputData << 0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0;
+    MatrixXd inputLabels(4, 2);
+    inputLabels << 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0, 0.0;
+    // in:   labels:
+    // 0 1    1
+    // 1 0    1
+    // 0 0    0
+    // 1 1    0
+
+    // Construct the Layers
+    FullyConnectedLayer firstLayer(2, 2);
+    FullyConnectedLayer secondLayer(2, 2);
+    SigmoidActivationLayer firstSigmoidLayer;
+    SigmoidActivationLayer secondSigmoidLayer;
+    //L2LossLayer LossLayer;
+    SoftmaxLossLayer LossLayer;
+
+    // // OPTIONAL Weights and biases made Public in case you want to set your own weights.
+    // bool vManualSetWeights = false;
+    // if (vManualSetWeights)
+    // {
+    //     MatrixXd vTrueWeightsFirst(2, 2);
+    //     MatrixXd vTrueWeightsSecond(2, 1);
+    //     MatrixXd vTrueBiasesFirst(1, 2);
+    //     MatrixXd vTrueBiasesSecond(1, 1);
+    //     vTrueWeightsFirst << -2.0, 1.0, -2.0, 1.0;
+    //     vTrueWeightsSecond << 1.08, 1.1;
+    //     vTrueBiasesFirst << 3.0, -0.5;
+    //     vTrueBiasesSecond << -1.5;
+    //     firstLayer.mWeights = vTrueWeightsFirst;
+    //     firstLayer.mBiases = vTrueBiasesFirst;
+    //     secondLayer.mWeights = vTrueWeightsSecond;
+    //     secondLayer.mBiases = vTrueBiasesSecond;
+    // }
+
+    // Construct network and train with parameters that seem to work.
+    // Since we are overfitting a dataset, no need for predicting a test set.
+    // Train method prints performance on the whole dataset (inputData,inputLabels) to see the evolution.
+    bool vManualCompute = true;
+
+    if (!vManualCompute)
+    {
+        // FullyConnectedNetwork vNetwork221({&firstLayer,
+        //                                    &secondLayer});
+        // vNetwork221.ConnectLayers();
+        // vNetwork221.Train(inputData, inputLabels, 0.05, 12001,false);
+    }
+    else
+    {
+        
+        firstSigmoidLayer.SetInput(firstLayer.GetOutput());
+        secondLayer.SetInput(firstSigmoidLayer.GetOutput());
+        secondSigmoidLayer.SetInput(secondLayer.GetOutput());
+        LossLayer.SetInput(secondSigmoidLayer.GetOutput());
+
+        firstLayer.SetBackpropInput(firstSigmoidLayer.GetBackpropOutput());
+        firstSigmoidLayer.SetBackpropInput(secondLayer.GetBackpropOutput());
+        secondLayer.SetBackpropInput(secondSigmoidLayer.GetBackpropOutput());
+        secondSigmoidLayer.SetBackpropInput(LossLayer.GetBackpropOutput());
+
+        firstLayer.mLearningRate = 0.05;
+        secondLayer.mLearningRate = 0.05;
+        // set input subset
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_int_distribution<> dis(0, inputData.rows() - 1);
+        const int vBatchSize = 2;
+        std::vector<int> vSampleIndices(vBatchSize);
+        for (size_t i = 0; i < 24001; i++)
+        {
+            for (size_t k = 0; k < vBatchSize; k++)
+            {
+                vSampleIndices[k] = dis(gen);
+            }
+
+            MatrixXd inputSample = inputData(vSampleIndices, Eigen::all);
+            MatrixXd inputSampleLabel = inputLabels(vSampleIndices, Eigen::all);
+            //std::cout << "---- firstLayer.SetInput ----" << std::endl;
+            firstLayer.SetInput(inputData);
+            LossLayer.SetLabels(inputLabels);
+            //std::cout << "---- firstLayer.ForwardPass() ----" << std::endl;
+            firstLayer.ForwardPass();
+            //std::cout << "---- firstSigmoidLayer.ForwardPass() ----" << std::endl;
+            firstSigmoidLayer.ForwardPass();
+            //std::cout << "---- secondLayer.ForwardPass() ----" << std::endl;
+            secondLayer.ForwardPass();
+            //std::cout << "---- secondSigmoidLayer.ForwardPass() ----" << std::endl;
+            secondSigmoidLayer.ForwardPass();
+            //std::cout << "---- secondLayer.ComputeLoss() ----" << std::endl;
+            LossLayer.ForwardPass();
+            //std::cout << "---- secondLayer.BackwardPass() ----" << std::endl;
+            LossLayer.BackwardPass();
+            //
+            secondSigmoidLayer.BackwardPass();
+            //
+            secondLayer.BackwardPass();
+            //std::cout << "---- firstSigmoidLayer.BackwardPass() ----" << std::endl;
+            firstSigmoidLayer.BackwardPass();
+            //std::cout << "---- firstLayer.BackwardPass() ----" << std::endl;
+            firstLayer.BackwardPass();
+            if (i % 100 == 0)
+            {
+                firstLayer.SetInput(inputData);
+                LossLayer.SetLabels(inputLabels);
+                firstLayer.ForwardPass();
+                firstSigmoidLayer.ForwardPass();
+                secondLayer.ForwardPass();
+                secondSigmoidLayer.ForwardPass();
+                LossLayer.ForwardPass();
+                std::cout << "----- Total Loss: ----" << std::endl;
+                std::cout << LossLayer.GetLoss() << std::endl;
+                std::cout << "----- Total Out: -----" << std::endl;
+                std::cout << *(secondSigmoidLayer.GetOutput()) << std::endl;
+                std::cout << "----- Total Labels: --" << std::endl;
+                std::cout << inputLabels << std::endl;
+            }
+        }
+    }
+
+
+    std::cout << "Rerun a few times (random weight initialization)" << std::endl;
+}
---
+++ include/libdl/SoftmaxLossLayer.h
@@ -0,0 +1,60 @@
+/** @file LossLayer.h
+ *  @author Adria Font Calvarons
+ */
+#ifndef SOFTMAXLOSSLAYER_H
+#define SOFTMAXLOSSLAYER_H
+
+#include "LossBaseLayer.h"
+/**
+@class SoftmaxLossLayer
+@brief Softmax Loss Layer.
+ */
+class SoftmaxLossLayer : public LossBaseLayer
+{
+protected:
+    MatrixXd mGradientHelper;
+
+public:
+    // Constructors
+    SoftmaxLossLayer();
+
+    void ForwardPass();
+    void BackwardPass();
+};
+
+SoftmaxLossLayer::SoftmaxLossLayer(){};
+
+void SoftmaxLossLayer::ForwardPass()
+{
+    //check same number of training samples.
+    const size_t vLabelNum = mLabels.rows();
+    const size_t vOutputNum = (*mInputPtr).rows();
+    if (vLabelNum == vOutputNum)
+    {
+        //Softmax
+        MatrixXd exp = (*mInputPtr).array().exp();
+        mGradientHelper = exp.array().colwise() / exp.rowwise().sum().array();
+        MatrixXd logprobs = -mGradientHelper.array().log();
+        MatrixXd filtered = logprobs.cwiseProduct(mLabels);
+        // std::cout << "(*mInputPtr)" << std::endl;
+        // std::cout << (*mInputPtr) << std::endl;
+        // std::cout << "exp" << std::endl;
+        // std::cout << exp << std::endl;
+        // std::cout << "probs" << std::endl;
+        // std::cout << mGradientHelper << std::endl;
+        // std::cout << logprobs << std::endl;
+        // std::cout << filtered << std::endl;
+        // Loss divided by number of examples
+        mLoss = filtered.array().sum() / static_cast<double>(vOutputNum);
+    }
+    else
+    {
+        throw(std::runtime_error("ComputeLoss(): dimension mismatch"));
+    }
+};
+void SoftmaxLossLayer::BackwardPass()
+{
+    mBackpropOutput = mGradientHelper - mLabels;
+    //std::cout << mBackpropOutput << " mBackpropOutput" << std::endl;
+};
+#endif
